aeb72656733d08cc443f8872d105e13634810434
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2022-07-28 22:12:23 +0600

    ./

---
+++ Conditional Statement/ConditionalStatements.cpp
@@ -0,0 +1,11 @@
+// Date 25 Jyly 2022
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+
+    return 0;
+}
---
+++ Input&Output/code.cpp
@@ -0,0 +1,31 @@
+//  Date : July 25 , 2022
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // string s{"Heelo"};
+    // int n;
+    // cin >> s >> n;
+    // for (char c : s)
+    // {
+    //     cout << "[" << c << "]";
+    // }
+
+    // cout << '\n';
+    string s;
+    int n;
+    cin >> s >> n;
+    int a, b, c, sum = 0;
+    cin >> a >> b >> c;
+    sum = a+ b + c;
+    cout << sum << endl;
+    return 0;
+}
